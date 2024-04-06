83a930ae92e19da8540644516958d4f0763520df
SMJin <sstaw@naver.com>
2024-04-06 15:50:28 +0900

    JDBC - 수정 (ResultSet)

--- 01_jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ 01_jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,30 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        // save
         Member member = new Member("memberV0", 10000);
         repositoryV0.save(member);
+
+        // findById
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember={}", member == findMember);
+        log.info("member equals findMember={}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
