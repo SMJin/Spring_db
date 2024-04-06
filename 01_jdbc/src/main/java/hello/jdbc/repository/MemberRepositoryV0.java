5f7f8276af10d462f6a082e37a069f1dafef22d4
SMJin <sstaw@naver.com>
2024-04-06 15:29:04 +0900

    DriverManager - 등록. (PreparedStatment')

---
+++ 01_jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,70 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            // SQL injection 을 예방하기 위해서는 PreparedStatement 를 이용해서 ? 로 바인딩 해주어야 한다.
+            pstmt = con.prepareStatement(sql);
+
+            // parameter binding
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+
+            // 실행 - 영향받은 row 수를 반환함(int)
+            pstmt.executeUpdate();
+
+            return member;
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
