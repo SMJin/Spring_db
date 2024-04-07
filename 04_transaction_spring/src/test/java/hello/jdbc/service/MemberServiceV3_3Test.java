f497572f3ce27ddcd9a6770f4b79b67afebc530b
SMJin <sstaw@naver.com>
2024-04-07 14:16:32 +0900

    @Transactional :: dataSource와 transactionManager 자동 등록.

--- 04_transaction_spring/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
+++ 04_transaction_spring/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -11,19 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
-import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * 트랜잭션 - @Transactional AOP
+ * 트랜잭션 - DataSource, transactionMangaer 자동 등록
  */
 @Slf4j
 @SpringBootTest
@@ -39,19 +36,14 @@ class MemberServiceV3_3Test {
 
     @TestConfiguration
     static class TestConfig {
-        @Bean
-        DataSource dataSource() {
-            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        }
-
-        @Bean
-        PlatformTransactionManager transactionManager() {
-            return new DataSourceTransactionManager(dataSource());
+        private final DataSource dataSource;
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
         }
 
         @Bean
         MemberRepositoryV3 memberRepositoryV3() {
-            return new MemberRepositoryV3(dataSource());
+            return new MemberRepositoryV3(dataSource);
         }
 
         @Bean
