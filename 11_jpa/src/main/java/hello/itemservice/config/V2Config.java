59b616e3168b39665bbf967b303967fb1295c88e
SMJin <sstaw@naver.com>
2024-04-14 20:00:13 +0900

    SpringDataJPA + QueryDSL

---
+++ 11_jpa/src/main/java/hello/itemservice/config/V2Config.java
@@ -0,0 +1,36 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import hello.itemservice.service.ItemServiceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class V2Config {
+
+    private final EntityManager em;
+    private final ItemRepositoryV2 itemRepositoryV2; // spring에서  기본으로 주어짐
+
+    @Bean
+    public ItemService service() {
+        return new ItemServiceV2(
+                itemRepositoryV2,
+                repository());
+    }
+
+    @Bean
+    public ItemQueryRepositoryV2 repository() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+}
